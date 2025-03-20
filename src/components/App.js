9bce74f51be5a1cf250bc3f4fc202616b036ccc9
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-03-20 21:01:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component, useEffect, useState } from "react";
 import WeatherDisplay from "./WeatherDisplay";
-import '../styles/App.css';
+// import '../styles/App.css';
 
 const App = () => {
-  const [temp, setTemp] = useState({ temperature: 0, conditions: "" });
+  const [temp, setTemp] = useState({ temperature: 25, conditions: "Sunny" });
 
   useEffect(() => {
     setTemp({ temperature: 25, conditions: "Sunny" });
